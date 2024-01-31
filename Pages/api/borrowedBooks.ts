e2b48d5215fa94bf75ec1bb7105425ab3d744da7
MyNameJaeff <fredrik.andersson@elev.ga.ntig.se>
2024-01-31 12:46:31 +0100

    Merged dashboard_backend to Backend

---
+++ Pages/api/borrowedBooks.ts
@@ -0,0 +1,43 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  prisma.$connect();
+  if (req.method === "POST") {
+    try {
+      if (req.body.userType === "staff") {
+        const response = await prisma.borrowedBooks.findMany({
+          where: {
+            staffId: req.body.userId,
+          },
+        });
+        res.status(200).json({ books: response });
+      } else {
+        const response = await prisma.borrowedBooks.findMany({
+          where: {
+            studentId: req.body.userId,
+          },
+        });
+        res.status(200).json({ books: response });
+      }
+    } catch (error) {
+      console.log(error);
+      res.status(404).json({ message: error });
+    }
+  } else {
+    try {
+      const response = await prisma.borrowedBooks.findMany();
+      res.status(200).json({ books: response });
+      prisma.$disconnect();
+    } catch (error) {
+      console.log(error);
+      res.status(404).json({ message: error });
+      prisma.$disconnect();
+    }
+  }
+}
---
+++ src/app/dashboard/available/page.tsx
@@ -0,0 +1,76 @@
+"use client";
+import React from "react";
+import { useRouter } from "next/navigation";
+import axios from "axios";
+
+const Page = () => {
+  const router = useRouter();
+  const [availableBooks, setAvailableBooks] = React.useState([]);
+
+  React.useEffect(() => {
+    const getBooks = async () => {
+      const response = await axios.get("http://localhost:3000/api/availableBooks");
+      setAvailableBooks(response.data.books);
+    };
+    getBooks();
+  }, []);
+
+  return (
+    <div>
+      <h1>Available to borrow Books</h1>
+      <table
+        className="table-auto"
+        style={{ width: 1000, textAlign: "center" }}
+      >
+        <thead>
+          <tr>
+            <th>Book Title</th>
+            <th>Author</th>
+            <th>Published</th>
+            <th>Registered At</th>
+          </tr>
+        </thead>
+        <tbody>
+          {availableBooks.map((book: any) =>
+            <tr key={book.invNr}>
+              <td>{`${book.title}`}</td>
+              <td>{`${book.author}`}</td>
+              <td>{`${(book.published).split("T")[0]}`}</td>
+              <td>{`${(book.regDate).split("T")[0] + " " + ((book.regDate).split("T")[1]).split(".")[0]}`}</td>{/* regDate currently does not exist, idk if added later but keep here for now */}
+            </tr>
+          )}
+        </tbody>
+      </table>
+      <div
+        style={{ width: 1000, display: "flex", justifyContent: "space-around" }}
+      >
+        <button
+          onClick={() => router.push("/dashboard/allbooks")}
+          style={{ backgroundColor: "lightblue", color: "black" }}
+        >
+          All Books
+        </button>
+        <button
+          onClick={() => router.push("/dashboard/available")}
+          style={{ backgroundColor: "lightblue", color: "black" }}
+        >
+          Available Books
+        </button>
+        <button
+          onClick={() => router.push("/dashboard/borrowed")}
+          style={{ backgroundColor: "lightblue", color: "black" }}
+        >
+          Borrowed Books
+        </button>
+        <button
+          onClick={() => router.push("/dashboard/missing")}
+          style={{ backgroundColor: "lightblue", color: "black" }}
+        >
+          Missing Books
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Page;
