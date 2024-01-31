1e0fbbb9a2e7b2b5b9c01a4ac8e008855c94e656
Albin Lundstrom <albin.lundstrom@elev.ntig.se>
2024-01-31 11:22:28 +0100

    prisma scheme. Student changes

---
+++ prisma/migrations/20240131102136_init/migration.sql
@@ -0,0 +1,135 @@
+-- CreateTable
+CREATE TABLE "Staff" (
+    "id" SERIAL NOT NULL,
+    "admin" BOOLEAN NOT NULL DEFAULT false,
+    "password" TEXT,
+    "name" TEXT,
+    "email" TEXT NOT NULL,
+    "phone" VARCHAR(13) NOT NULL,
+    "image" VARCHAR(255) NOT NULL,
+
+    CONSTRAINT "Staff_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Student" (
+    "id" SERIAL NOT NULL,
+    "password" TEXT,
+    "name" TEXT,
+    "email" TEXT NOT NULL,
+    "phone" VARCHAR(13) NOT NULL,
+    "image" VARCHAR(255) NOT NULL,
+
+    CONSTRAINT "Student_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Book" (
+    "id" SERIAL NOT NULL,
+    "bookImg" VARCHAR(255) NOT NULL,
+    "title" TEXT,
+    "author" TEXT,
+    "publishers" TEXT,
+    "published" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "regDate" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "isbn" TEXT,
+    "invNr" INTEGER NOT NULL,
+    "price" INTEGER NOT NULL,
+    "available" BOOLEAN NOT NULL DEFAULT true,
+
+    CONSTRAINT "Book_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "missingBooks" (
+    "id" SERIAL NOT NULL,
+    "regDate" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "note" VARCHAR(255) NOT NULL,
+    "bookId" INTEGER,
+    "staffId" INTEGER,
+    "studentId" INTEGER,
+
+    CONSTRAINT "missingBooks_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "borrowedBooks" (
+    "id" SERIAL NOT NULL,
+    "regDate" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "note" VARCHAR(255) NOT NULL,
+    "bookId" INTEGER,
+    "staffId" INTEGER,
+    "studentId" INTEGER,
+
+    CONSTRAINT "borrowedBooks_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "returnedBooks" (
+    "id" SERIAL NOT NULL,
+    "regDate" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "bookId" INTEGER,
+    "staffId" INTEGER,
+    "studentId" INTEGER,
+
+    CONSTRAINT "returnedBooks_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "loanedBooks" (
+    "id" SERIAL NOT NULL,
+    "regDate" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "bookId" INTEGER,
+    "staffId" INTEGER,
+    "studentId" INTEGER,
+
+    CONSTRAINT "loanedBooks_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Staff_email_key" ON "Staff"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Staff_phone_key" ON "Staff"("phone");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Student_email_key" ON "Student"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Student_phone_key" ON "Student"("phone");
+
+-- AddForeignKey
+ALTER TABLE "missingBooks" ADD CONSTRAINT "missingBooks_bookId_fkey" FOREIGN KEY ("bookId") REFERENCES "Book"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "missingBooks" ADD CONSTRAINT "missingBooks_staffId_fkey" FOREIGN KEY ("staffId") REFERENCES "Staff"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "missingBooks" ADD CONSTRAINT "missingBooks_studentId_fkey" FOREIGN KEY ("studentId") REFERENCES "Student"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "borrowedBooks" ADD CONSTRAINT "borrowedBooks_bookId_fkey" FOREIGN KEY ("bookId") REFERENCES "Book"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "borrowedBooks" ADD CONSTRAINT "borrowedBooks_staffId_fkey" FOREIGN KEY ("staffId") REFERENCES "Staff"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "borrowedBooks" ADD CONSTRAINT "borrowedBooks_studentId_fkey" FOREIGN KEY ("studentId") REFERENCES "Student"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "returnedBooks" ADD CONSTRAINT "returnedBooks_bookId_fkey" FOREIGN KEY ("bookId") REFERENCES "Book"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "returnedBooks" ADD CONSTRAINT "returnedBooks_staffId_fkey" FOREIGN KEY ("staffId") REFERENCES "Staff"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "returnedBooks" ADD CONSTRAINT "returnedBooks_studentId_fkey" FOREIGN KEY ("studentId") REFERENCES "Student"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "loanedBooks" ADD CONSTRAINT "loanedBooks_bookId_fkey" FOREIGN KEY ("bookId") REFERENCES "Book"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "loanedBooks" ADD CONSTRAINT "loanedBooks_staffId_fkey" FOREIGN KEY ("staffId") REFERENCES "Staff"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "loanedBooks" ADD CONSTRAINT "loanedBooks_studentId_fkey" FOREIGN KEY ("studentId") REFERENCES "Student"("id") ON DELETE SET NULL ON UPDATE CASCADE;
