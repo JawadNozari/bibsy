d65ab0a3d08eef9a9b730279220b6620846fe364
Greed <106820018+maxberglund2@users.noreply.github.com>
2024-01-31 15:20:33 +0100

    getUsers Type fix

--- Pages/api/getUsers.ts
+++ Pages/api/getUsers.ts
@@ -1,8 +1,12 @@
+import type { NextApiRequest, NextApiResponse } from "next";
 import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
-  export default async function handler(req, res) {
+  export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse
+  ) {
     try {
       const staffUsers = await prisma.Staff.findMany();
       const studentUsers = await prisma.Student.findMany();
