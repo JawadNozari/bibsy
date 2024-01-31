84f59edefba36d282065a281d29923f13d32922d
Albin Lundstrom <albin.lundstrom@elev.ntig.se>
2024-01-31 12:31:39 +0100

    db uppdate

---
+++ Pages/api/getStaff.ts
@@ -0,0 +1,15 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+export default async function getStaff() {
+  try {
+    const allUsers = await prisma.Staff.findMany();
+    return(allUsers)
+  } catch (error) {
+    console.error(error);
+    throw error; // Throw the error to be caught in the catch block below
+  } finally {
+    await prisma.$disconnect();
+  }
+}
