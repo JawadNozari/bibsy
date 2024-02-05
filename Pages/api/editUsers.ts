1b6b83f7c4e32ce6c214efd18a5561ce61ab19f7
Albin Lundstrom <albin.lundstrom@elev.ntig.se>
2024-02-05 11:48:16 +0100

    Edit Users

--- Pages/api/editUsers.ts
+++ Pages/api/editUsers.ts
@@ -7,26 +7,35 @@ const prisma = new PrismaClient();
 export default async function edit(req: NextApiRequest) {
   if (req.method === "POST") {
     try {
+      console.log(req.body);
+      const{first, last, password, email, phone, qrCode, studentclass} = req.body;
+      const admin = Boolean(req.body.Admin);
       if (req.body.userType === "staff") {
+        console.log("Staff");
       await prisma.staff.update({
         where: { id: req.body.id },
         data: { 
-          firstName:req.body.firstname,
-          lastName:req.body.lastname,
-          password:req.body.password,
-          email:req.body.email,
-          phone:req.body.phone 
+          firstName: first,
+          lastName: last,
+          password: password,
+          email: email,
+          phone: phone, 
+          qrCode: qrCode,
+          admin: admin
         },
       });
       }else {
+        console.log("Student");
         await prisma.student.update({
           where: { id: req.body.id },
           data: { 
-            firstName:req.body.firstname,
-            lastName:req.body.lastname,
-            password:req.body.password,
-            email:req.body.email,
-            phone:req.body.phone 
+            firstName: first,
+            lastName: last,
+            password: password,
+            email: email,
+            phone: phone,
+            qrCode: qrCode,
+            class: studentclass
           },
         });
       }
