ae4b42bb74c36068c63fb78577797069ee2904fe
Albin Lundstrom <albin.lundstrom@elev.ntig.se>
2024-02-05 10:31:57 +0100

    Edit Book

--- Pages/api/editBooks.ts
+++ Pages/api/editBooks.ts
@@ -1,17 +1,19 @@
 import { PrismaClient } from "@prisma/client";
-import type { NextApiRequest, NextApiResponse } from "next";
+import type { NextApiRequest } from "next";
 
 const prisma = new PrismaClient();
 
 
-export default async function edit(req: NextApiRequest, res: NextApiResponse) {
+export default async function edit(req: NextApiRequest) {
   if (req.method === "POST") {
     try {
         console.log(req.body);
         const{title, author, publisher, invNr, price, image} = req.body;
         const isbn = Number(req.body.isbn);
         const edit = await prisma.book.update({
-            where: { id: req.body.id },
+            where: { 
+                id: req.body.id 
+            },
             data: { 
                 title:title,
                 author:author,
