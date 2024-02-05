638e1b36c2501a2e19191554ba8f26b8937baf69
Jonatan Björklin <jonatan.bjorklin@elev.ntig.se>
2024-02-05 10:31:50 +0100

    adminCenter fix to create Student

--- Pages/api/adminCenter.ts
+++ Pages/api/adminCenter.ts
@@ -10,6 +10,7 @@ type Data = {
     phone: string,
     image: string,
     class: string,
+    qrCode: string,
 };
 
 // Handler function for handling student registration
@@ -30,6 +31,7 @@ export default async function adminCenter(req: NextApiRequest, res: NextApiRespo
                     phone,
                     image,
                     class: studentClass,
+                    qrCode: ""
                 },
             });
 
