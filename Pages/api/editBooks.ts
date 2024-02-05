1ed57f6f41bc744bb5df1eb8cd282ebb5306274c
Albin Lundstrom <albin.lundstrom@elev.ntig.se>
2024-02-05 09:24:47 +0100

    Edit Book func

--- Pages/api/editBooks.ts
+++ Pages/api/editBooks.ts
@@ -1,13 +1,14 @@
 import { PrismaClient } from "@prisma/client";
-import type { NextApiRequest } from "next";
+import type { NextApiRequest, NextApiResponse } from "next";
 
 const prisma = new PrismaClient();
 
 
-export default async function edit(req: NextApiRequest) {
+export default async function edit(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === "POST") {
     try {
-        await prisma.book.update({
+        console.log(req.body);
+        const edit = await prisma.book.update({
             where: { id: req.body.id },
             data: { 
                 title: req.body.title,
@@ -18,8 +19,9 @@ export default async function edit(req: NextApiRequest) {
                 price: req.body.price,
             },
         });
+        console.log(edit);
     }catch {
-      console.log(Error);
+        console.log(Error);
     }
   }
 }
---
+++ src/app/EditTest/page.tsx
@@ -0,0 +1,78 @@
+"use client";
+import { useState } from "react";
+import axios from "axios";
+
+export default function Page() {
+	const [title, setUsername] = useState<string>("");
+	const [author, setPassword] = useState<string>("");
+	const [publisher, setRemember] = useState<string>("");
+	const [isbn, setIsbn] = useState<string>("");
+
+	const handleUsernameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setUsername(e.target.value);
+	};
+	const handleAuthorChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setPassword(e.target.value);
+	};
+	const handleIsbnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setIsbn(e.target.value);
+	};
+	const handleRememberChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setRemember(e.target.value);
+	};
+
+
+    const handleSubmit = (e: React.SyntheticEvent) => {
+        e.preventDefault();
+        return axios.post("/api/editBooks", {
+          id: 1,
+          title: title,
+          author: author,
+          publisher: publisher,
+          isbn: isbn,
+          invNr: 321,
+          price: 12,
+          image: "TestBild.png"
+          
+        });
+    };
+  
+	return (
+        <form onSubmit={handleSubmit} >
+            <input
+                type="text"
+                value={title}
+                onChange={handleUsernameChange}
+                placeholder="title"
+                id="username"
+            />
+            <input
+                type="text"
+                value={author}
+                onChange={handleAuthorChange}
+                placeholder="author"
+                id="username"
+            />
+            <input
+                type="text"
+                value={isbn}
+                onChange={handleIsbnChange}
+                placeholder="isbn"
+                id="username"
+            />
+            <div >
+                <input
+                    type="text"
+                    value={publisher}
+                    onChange={handleRememberChange}
+                    placeholder="publisher"
+                    id="publisher"
+                />
+            </div>
+            <button
+                type="submit">
+                    edit
+            </button>
+        </form>
+	);
+}
