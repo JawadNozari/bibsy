3f87cd9a45f2af237c744a503274381660f4f1b3
Jawad Nozari <mjawad.nozari@gmail.com>
2024-02-06 00:54:51 +0100

    Update depricated_login.ts

--- Pages/api/depricated_login.ts
+++ Pages/api/depricated_login.ts
@@ -1,54 +1,51 @@
 import { PrismaClient } from "@prisma/client";
 import { NextApiRequest, NextApiResponse } from "next";
-import jwt from "jsonwebtoken";
 
 const prisma = new PrismaClient();
 
-export default async function main(
-  req: NextApiRequest, 
-  res: NextApiResponse
-  ) {
-   try {
+export default async function main(req: NextApiRequest, res: NextApiResponse) {
+	//  try {
 
-      var userInfo = {
-        username: req.body.userCredentials.username, // USERNAME IS EMAIL
-        password: req.body.userCredentials.password,
-        remember: req.body.remember
-      };
+	//     var userInfo = {
+	//       username: req.body.userCredentials.username, // USERNAME IS EMAIL
+	//       password: req.body.userCredentials.password,
+	//       remember: req.body.remember
+	//     };
 
-      const checkLoginStaff = await prisma.staff.findFirst({
-        where: {
-          AND: [
-            { email: userInfo.username },
-            { password: userInfo.password},
-            { admin: true }
-          ]
-        }
-      });
+	//     const checkLoginStaff = await prisma.staff.findFirst({
+	//       where: {
+	//         AND: [
+	//           { email: userInfo.username },
+	//           { password: userInfo.password},
+	//           { admin: true }
+	//         ]
+	//       }
+	//     });
 
-      const checkLoginStudent = await prisma.student.findFirst({
-        where: {
-          AND: [
-            { email: userInfo.username },
-            { password: userInfo.password }
-          ]
-        }
-      });
+	//     const checkLoginStudent = await prisma.student.findFirst({
+	//       where: {
+	//         AND: [
+	//           { email: userInfo.username },
+	//           { password: userInfo.password }
+	//         ]
+	//       }
+	//     });
 
-      console.log(checkLoginStudent);
+	//     console.log(checkLoginStudent);
 
-      if (checkLoginStaff || checkLoginStudent) {
-        const token = jwt.sign({ username: userInfo.username, timestamp: Date.now() }, "your-secret-key", { expiresIn: "1h" });
-        res.status(200).json({ token });
-      } else {
-        res.status(401).json({ error: "Invalid credentials" });
-      }
+	//     if (checkLoginStaff || checkLoginStudent) {
+	//       const token = jwt.sign({ username: userInfo.username, timestamp: Date.now() }, "your-secret-key", { expiresIn: "1h" });
+	//       res.status(200).json({ token });
+	//     } else {
+	//       res.status(401).json({ error: "Invalid credentials" });
+	//     }
 
-      res.status(200);
+	//     res.status(200);
 
-    } catch (error) {
-      res.status(500).json({ error: "Internal Server Error" });
-    } finally {
-      await prisma.$disconnect();
-  }
+	//   } catch (error) {
+	//     res.status(500).json({ error: "Internal Server Error" });
+	//   } finally {
+	//     await prisma.$disconnect();
+	// }
+	res.status(500).json({ error: "Depricated Code" });
 }
