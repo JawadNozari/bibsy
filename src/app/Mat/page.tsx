407cc3398400e0c216c5fdd1152158205ecf0b23
WixdomCoding <106668839+WixdomCoding@users.noreply.github.com>
2024-02-07 11:36:54 +0100

    Finalized mapping of the mat page

--- src/app/Mat/page.tsx
+++ src/app/Mat/page.tsx
@@ -21,7 +21,7 @@ export default function Page() {
 	return (
 		<div>
 			<div className="flex items-center gap-10 grid-cols-2 place-content-center w-screen h-screen bg-indigo-100">
-				<div className="flex flex-col rounded shadow-lg h-3/4 bg-white text-violet-500 w-1/3 place-content-center ">
+				<div className="flex flex-col rounded-lg shadow-lg h-3/4 bg-white text-violet-500 w-1/3 place-content-center ">
 					<div className="flex flex-col  items-center h-1/4 gap-10 pt-8">
 						<h1 className="text-4xl">Time</h1>
 						<h2 className="text-3xl">Today's Lunch</h2>
@@ -35,28 +35,28 @@ export default function Page() {
 						</div>
 					</div>
 
-					<div className="flex flex-row place-content-center items-end h-1/4 gap-2 pb-8">
+					<div className="flex flex-row place-content-center items-end h-1/4 gap-2 pb-6">
 						<p>Icon img här</p>
 						<h4 className="text-xl">Closing in:</h4>
 						<h3 className="text-xl">Tid här</h3>
 					</div>
 				</div>
-				<div className="flex flex-col rounded shadow-lg h-3/4 bg-white text-black w-3/5 place-content-center items-center">
-					<div>
-						<h1>Hello 2</h1>
+				<div className="flex flex-col rounded-lg shadow-lg h-3/4 bg-white text-violet-500 w-3/5 items-center">
+					<div className="flex flex-col items-center h-1/4 gap-10 pt-8 ">
+						<h1 className="text-4xl">NTI GYMNASIET</h1>
+						<h2 className="text-3xl">Last Check In</h2>
 					</div>
-					<div>
-						<h2>Last Check In</h2>
-					</div>
-					<div className="flex items-center gap-10 grid-cols-2 place-content-center">
-						<div className="mr-10">
-							<h3>Students</h3>
+					<div className="flex gap-[200%] grid-cols-2 mt-10 h-2/4 place-content-center">
+						<div>
+							<h3 className="text-2xl">Students</h3>
 						</div>
-						<div className="ml-10">
-							<h3>Teachers</h3>
+						<div>
+							<h3 className="text-2xl">Teachers</h3>
 						</div>
 					</div>
-					<div>test</div>
+					<div className="flex flex-row h-1/4 items-end pb-6">
+						<p>(Check here) (Number) People checked in so far</p>
+					</div>
 				</div>
 			</div>
 		</div>
