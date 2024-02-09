2d149cffb4f0792b63ef3c8b27e9f34288240b78
Tayfun <ripeaglee@hotmail.com>
2024-02-09 15:31:46 +0100

    book register DONE i guess

--- src/app/api/bookReg/route.ts
+++ src/app/api/bookReg/route.ts
@@ -3,62 +3,77 @@ import path from "path";
 import { writeFile, mkdir } from "fs/promises";
 import { PrismaClient } from "@prisma/client";
 
-export const GET = async () => {
-	return NextResponse.json({ message: "Hello World" });
-};
-
 const prisma = new PrismaClient();
 
 export const POST = async (req: NextRequest) => {
+	// Get form data from the request
 	const formData = await req.formData();
 	const file = formData.get("file");
 
-	if (!file) {
-		return NextResponse.json({ error: "No files received." }, { status: 400 });
-	}
+	let bookImgPath = "noBookCover/no-cover.jpg";
 
-	const buffer = Buffer.from(await (file as File).arrayBuffer());
-	const filename = (file as File).name.replaceAll(" ", "_");
+	if (file) {
+		const buffer = Buffer.from(await (file as File).arrayBuffer());
+		const filename = (file as File).name.replaceAll(" ", "_");
+
+		// Directory where the file will be uploaded
+		const uploadDirectory = path.join(process.cwd(), "public/UploadedImage");
+
+		// Create the directory if it doesn't exist
+		await mkdir(uploadDirectory, { recursive: true });
+		// Write the file to the directory
+		await writeFile(path.join(uploadDirectory, filename), buffer);
+
+		// Update the book image path
+		bookImgPath = `UploadedImage/${filename}`;
+	} else if (!file && formData.get("imageUrl")) {
+		// If no file is received but there is a URL, use the URL as the book cover
+		bookImgPath = formData.get("imageUrl") as string;
+	} else {
+		// use this as default image if no file or URL is received
+		bookImgPath = "noBookCover/no-cover.jpg";
+	}
 
 	const title = formData.get("title") as string;
 	const author = formData.get("author") as string;
 	const publishers = formData.get("publishers") as string;
 	const published = new Date(formData.get("published") as unknown as string);
-	const isbn = Number(formData.get("isbn"));
+	const isbn = formData.get("isbn");
 	const invNr = Number(formData.get("invNr"));
 	const price = Number(formData.get("price"));
-	const uploadDirectory = path.join(process.cwd(), "public/UploadedImage");
 
-	// Create the directory if it doesn't exist
-	await mkdir(uploadDirectory, { recursive: true });
-	await writeFile(path.join(uploadDirectory, filename), buffer);
-	// Save file and form data to the database using Prisma
+	// Save file and form data to the database
 	const resp = await prisma.book
 		.create({
 			data: {
-				bookImg: `UploadedImage/${filename}`,
+				bookImg: bookImgPath,
 				title,
 				author,
 				publishers,
 				published,
-				isbn,
+				isbn: isbn?.toString() || "",
 				invNr,
 				price,
 			},
 		})
 		.then((book) => {
+			// Disconnect Prisma client after successful creation
 			prisma.$disconnect();
 			return {
-				Message: `${filename} and form data saved successfully`,
+				Message: "Form data saved successfully",
 				status: 201,
 				book,
 			};
 		})
 		.catch((error: unknown) => {
+			// Handle errors
 			return { Message: error, status: 500 };
 		})
 		.finally(() => {
 			prisma.$disconnect();
 		});
+
+	console.log(resp);
+
 	return NextResponse.json(resp);
 };
