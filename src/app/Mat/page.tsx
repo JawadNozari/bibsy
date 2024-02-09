fee618e5bffa8fd32032641eeb6c7788947a2cec
WixdomCoding <106668839+WixdomCoding@users.noreply.github.com>
2024-02-09 12:04:21 +0100

    Changed some things in mat, added global font

--- src/app/Mat/page.tsx
+++ src/app/Mat/page.tsx
@@ -3,8 +3,16 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 export default function Page() {
-	const [currentLunch, setCurrentLunch] = useState(null);
-	const [currentVegetarian, setCurrentVegetarian] = useState(null);
+	const [currentLunch, setCurrentLunch] = useState<string | null>(null);
+	const [currentVegetarian, setCurrentVegetarian] = useState<string | null>(
+		null,
+	);
+	const [closingTime, setClosingTime] = useState<Date | null>(null);
+	const [currentTime, setCurrentTime] = useState<string>("");
+	const [lunchTime, setLunchTime] = useState<string | null>(null);
+	const [closingIn, setClosingIn] = useState<string | JSX.Element>(
+		"Currently closed",
+	);
 
 	useEffect(() => {
 		axios
@@ -17,13 +25,61 @@ export default function Page() {
 			.catch((err) => {
 				console.log("error", err.code);
 			});
+
+		// Set closing time to 13:00 for today
+		const today = new Date();
+		const closingTimeToday = new Date(today);
+		const openingTimeToday = new Date(today);
+		closingTimeToday.setHours(13, 0, 0, 0);
+		openingTimeToday.setHours(11, 0, 0, 0);
+
+		if (today.getHours() >= 11 && today.getHours() < 13) {
+			setClosingTime(closingTimeToday);
+		} else {
+			setClosingIn("Currently closed");
+		}
+
+		// Start interval to update current time
+		const interval = setInterval(() => {
+			const date = new Date();
+			const timeString = date.toLocaleTimeString("sv-SE", {
+				hour: "2-digit",
+				minute: "2-digit",
+			});
+			setCurrentTime(timeString);
+		}, 1000);
+
+		// Clear interval on component unmount
+		return () => clearInterval(interval);
 	}, []);
+
+	useEffect(() => {
+		const interval = setInterval(() => {
+			const now = new Date();
+			if (closingTime) {
+				const timeRemaining = closingTime.getTime() - now.getTime();
+				if (timeRemaining <= 0) {
+					clearInterval(interval);
+					// Lunchtime is over
+					setClosingTime(null);
+					setClosingIn("Currently closed");
+				} else {
+					const hours = Math.floor((timeRemaining / (1000 * 60 * 60)) % 24);
+					const minutes = Math.floor((timeRemaining / 1000 / 60) % 60);
+					setClosingIn(`${hours}h ${minutes}m`);
+				}
+			}
+		}, 1000);
+
+		return () => clearInterval(interval);
+	}, [closingTime]);
+
 	return (
 		<div>
 			<div className="flex items-center gap-10 grid-cols-2 place-content-center w-screen h-screen bg-indigo-100">
 				<div className="flex flex-col rounded-lg shadow-lg h-3/4 bg-white text-violet-500 w-1/3 place-content-center ">
 					<div className="flex flex-col  items-center h-1/4 gap-10 pt-8">
-						<h1 className="text-4xl">Time</h1>
+						<h1 className="text-4xl">{currentTime}</h1>
 						<h2 className="text-3xl">Today's Lunch</h2>
 					</div>
 					<div className="flex place-content-center items-center h-1/2 mt-10">
@@ -36,14 +92,18 @@ export default function Page() {
 					</div>
 
 					<div className="flex flex-row place-content-center items-end h-1/4 gap-2 pb-6">
-						<img className="w-8" src="./icons/timer.png" alt="timer icon"/>
+						<img className="w-8" src="./icons/timer.png" alt="timer icon" />
 						<h4 className="text-xl">Closing in:</h4>
-						<h3 className="text-xl">Tid här</h3>
+						<h3 className="text-xl">{closingIn}</h3>
 					</div>
 				</div>
 				<div className="flex flex-col rounded-lg shadow-lg h-3/4 bg-white text-violet-500 w-3/5 items-center">
 					<div className="flex flex-col items-center h-1/4 gap-10 pt-8 ">
-						<img className="w-1/2" src="./icons/nti_logo.png" alt="Nti gymnasiet logo" />
+						<img
+							className="w-1/2"
+							src="./icons/nti_logo.png"
+							alt="Nti gymnasiet logo"
+						/>
 						<h2 className="text-3xl -mt-1">Last Check In</h2>
 					</div>
 					<div className="flex gap-[200%] grid-cols-2 mt-10 h-2/4 place-content-center">
@@ -55,8 +115,13 @@ export default function Page() {
 						</div>
 					</div>
 					<div className="flex flex-row h-1/4 text-xl gap-2 items-end pb-6">
-                        <img className="w-8" src="./icons/checkmark.png" alt="checkmark icon"/>
-						<p>(Number) People checked in so far</p>
+						<img
+							className="w-8"
+							src="./icons/checkmark.png"
+							alt="checkmark icon"
+						/>
+						<b>(Number)</b>
+						<p>People checked in so far</p>
 					</div>
 				</div>
 			</div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,4 @@
-import type { Config } from "tailwindcss"
+import type { Config } from "tailwindcss";
 
 const config: Config = {
 	content: [
@@ -8,6 +8,9 @@ const config: Config = {
 		"./src/**/*.{js,ts,jsx,tsx,mdx}",
 	],
 	theme: {
+		fontFamily: {
+			Oxygen: ["Oxygen"],
+		},
 		extend: {
 			backgroundImage: {
 				"gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
@@ -36,10 +39,10 @@ const config: Config = {
 					"-ms-overflow-style": "var(--tw-gradient-stops)",
 					"scrollbar-width": "none",
 				},
-			}
-			addUtilities(newUtilities)
+			};
+			addUtilities(newUtilities);
 		},
 		require("daisyui"),
 	],
-}
-export default config
+};
+export default config;
