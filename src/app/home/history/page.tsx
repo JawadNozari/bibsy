b1dc920c3a206db3c6f54f54a9364821ad311d0d
MyNameJaeff <fredrik.andersson@elev.ga.ntig.se>
2024-02-12 15:49:13 +0100

    history page code style and comments

--- src/app/home/history/page.tsx
+++ src/app/home/history/page.tsx
@@ -11,6 +11,7 @@ const Page = () => {
     const [students, setStudents] = React.useState<Student[]>([]);
     const [staff, setStaff] = React.useState<Staff[]>([]);
 
+    //* Gets all of the books and sets the state for each type of book (registered, history) and all of the users
     React.useEffect(() => {
         const getHistory = async () => {
             const response = await axios("/api/getHistory");
@@ -25,6 +26,51 @@ const Page = () => {
         };
         getHistory();
     }, []);
+
+    //* Returns the JSX for the page with the history of borrowed books and buttons to navigate to different pages
+    const runThroughBooks = () => {
+        return (history ? history.map((book) => {
+            return books.map((registeredBook) => {
+                if (book.bookId === registeredBook.id) {
+                    return (
+                        // biome-ignore lint/a11y/useKeyWithClickEvents: <explanation>
+                        <tr
+                            className="bg-white border-b dark:bg-gray-600 dark:border-gray-500 hover:bg-gray-50 dark:hover:bg-gray-500 cursor-pointer active:bg-gray-200 dark:active:bg-gray-700"
+                            key={book.id}
+                            onClick={() =>
+                                router.push(`/home/bookDetails/${registeredBook.invNr}`)
+                            }
+                            tabIndex={0}
+                        >
+                            <th
+                                scope="row"
+                                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                            >{`${registeredBook.title}`}</th>
+                            <td className="px-6 py-4">{registeredBook.author}</td>
+                            {/* //suggestion: We need code cleanup here */}
+                            <td className="px-6 py-4">{`${(book.regDate).toString().split("T")[0]
+                                } ${(book.regDate).toString().split("T")[1].split(".")[0]}`}</td>
+                            {/* //* Runs through the users to fetch their names */}
+                            <td className="px-6 py-4">{
+                                staff.map((staffMember) => {
+                                    if (staffMember.id === book.staffId) {
+                                        return `${staffMember.firstName} ${staffMember.lastName} | ID: ${staffMember.id}`;
+                                    }
+                                })
+                            }</td>
+                            <td className="px-6 py-4">{
+                                students.map((student) => {
+                                    if (student.id === book.studentId) {
+                                        return `${student.firstName} ${student.lastName} | ID: ${student.id}`;
+                                    }
+                                })
+                            }</td>
+                        </tr>
+                    );
+                }
+            });
+        }) : "");
+    };
 
     return (
         <div className="flex min-h-screen flex-col items-center justify-between  p-24 bg-neutral-50">
@@ -53,46 +99,7 @@ const Page = () => {
                         </tr>
                     </thead>
                     <tbody>
-                        {history ? history.map((book) => {
-                            return books.map((registeredBook) => {
-                                if (book.bookId === registeredBook.id) {
-                                    return (
-                                        // biome-ignore lint/a11y/useKeyWithClickEvents: <explanation>
-                                        <tr
-                                            className="bg-white border-b dark:bg-gray-600 dark:border-gray-500 hover:bg-gray-50 dark:hover:bg-gray-500 cursor-pointer active:bg-gray-200 dark:active:bg-gray-700"
-                                            key={book.id}
-                                            onClick={() =>
-                                                router.push(`/home/bookDetails/${registeredBook.invNr}`)
-                                            }
-                                            tabIndex={0}
-                                        >
-                                            <th
-                                                scope="row"
-                                                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                                            >{`${registeredBook.title}`}</th>
-                                            <td className="px-6 py-4">{registeredBook.author}</td>
-                                            {/* //suggestion: We need code cleanup here */}
-                                            <td className="px-6 py-4">{`${(book.regDate).toString().split("T")[0]
-                                                } ${(book.regDate).toString().split("T")[1].split(".")[0]}`}</td>
-                                            <td className="px-6 py-4">{
-                                                staff.map((staffMember) => {
-                                                    if (staffMember.id === book.staffId) {
-                                                        return `${staffMember.firstName} ${staffMember.lastName} | ID: ${staffMember.id}`;
-                                                    }
-                                                })
-                                            }</td>
-                                            <td className="px-6 py-4">{
-                                                students.map((student) => {
-                                                    if (student.id === book.studentId) {
-                                                        return `${student.firstName} ${student.lastName} | ID: ${student.id}`;
-                                                    }
-                                                })
-                                            }</td>
-                                        </tr>
-                                    );
-                                }
-                            });
-                        }) : ""}
+                        {runThroughBooks()}
                     </tbody>
                 </table>
             </div>
