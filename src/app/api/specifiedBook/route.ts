8d3c26e2dc2e13d1aa6d4dbb9132bf92cafa614d
Albin Lundstrom <albin.lundstrom@elev.ntig.se>
2024-02-14 15:30:25 +0100

    Specifed books for sepecified users 2.0

--- src/app/api/specifiedBook/route.ts
+++ src/app/api/specifiedBook/route.ts
@@ -1,15 +1,24 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient, borrowedBooks } from "@prisma/client";
 import { NextResponse, NextRequest } from "next/server";
 
 const prisma = new PrismaClient();
 
-const fetchbook = async (id: number) => {
+const fetchbook = async (id: number, bookdata: Array<object>) => {
 	try {
-			const book = await prisma.book
-				.findUnique({
-					where: { id: id },
-				});
-				return book;
+		const book = await prisma.book.findUnique({
+			where: { id: id },
+		});
+		if (book !== null) {
+			bookdata.push(book);
+		} else {
+			return NextResponse.json(
+				{
+					message:
+						"Validation Error. This can be cuased becuase of mallformed Data (Check Data Type)",
+				},
+				{ status: 500 },
+			);
+		}
 	} catch {
 		// Extra error checking
 		console.debug(Error);
@@ -24,8 +33,11 @@ const fetchbook = async (id: number) => {
 };
 export const POST = async (req: NextRequest) => {
 	try {
-		const { id } = await req.json();
-		const bookdata = await fetchbook(id);
+		const { books } = await req.json();
+		const bookdata: object[] = [];
+		books.map(async (book: borrowedBooks) => {
+			await fetchbook(book.id, bookdata);
+		});
 		return NextResponse.json(bookdata);
 	} catch (error) {
 		// If there is an error, return the error
